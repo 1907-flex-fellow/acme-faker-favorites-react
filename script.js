5291b95e738a8820f97a5633bfc74371b144ac82
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-08-01 17:46:06 -0400

    functionality working and waiting for css

---
+++ script.js
@@ -0,0 +1,42 @@
+const root = document.querySelector('#root');
+
+
+class App extends React.Component{
+    constructor(){
+        super()
+        const users = []
+        while(users.length < 10){
+            users.push(faker.helpers.createCard())
+        }
+        this.state = {users}
+    }
+    render(){
+        const { users } = this.state;
+        const createUser = () => {
+            console.log('createUser');
+            const newUser = faker.helpers.createCard();
+            users.push({...newUser, isFavorite: true});
+            this.setState({users})
+        }
+        const toggleUser = (idx) => {
+            console.log('toggle user');
+            users[idx].isFavorite = !users[idx].isFavorite;
+            this.setState({users});
+        }
+
+        const lis = users.map( (user,idx) => {
+            const userCards = [user.name, user.username].map((item, idx) => React.createElement('div', {key: idx}, item))
+            return React.createElement('li', {onClick: () => {toggleUser(idx)}, key: idx}, userCards) 
+        } )
+        const favNum = users.filter( user => user.isFavorite ).length
+        const userList = React.createElement('ul', null, lis);
+        const subtitle = React.createElement('h2', {onClick: createUser}, `you have ${favNum} favorites`);
+        return (
+            React.createElement('div', null, subtitle, userList)
+        )
+    }
+}
+
+
+
+ReactDOM.render(React.createElement(App), root)
